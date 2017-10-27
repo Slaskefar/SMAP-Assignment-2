feb7c40bfb81a2a7105e16618463957bdc2fba3f
Kasper Lauge Madsen <kasperlaugemadsen@gmail.com>
2017-10-27 13:20:28 +0200

    Added methods for activities and fixed ui bug

--- app/src/main/java/com/example/jeppe/weatherapp/CityDetailsActivity.java
+++ app/src/main/java/com/example/jeppe/weatherapp/CityDetailsActivity.java
@@ -2,12 +2,37 @@ package com.example.jeppe.weatherapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CityDetailsActivity extends AppCompatActivity {
-
+    Button btnRemove;
+    Button btnOk;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_city_details);
+
+        btnRemove = findViewById(R.id.btnRemove);
+        btnRemove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                removeCity();
+            }
+        });
+
+        btnOk = findViewById(R.id.btnOk);
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ok();
+            }
+        });
+    }
+
+    private void removeCity() {
+    }
+
+    private void ok() {
     }
 }
--- app/src/main/java/com/example/jeppe/weatherapp/CityListActivity.java
+++ app/src/main/java/com/example/jeppe/weatherapp/CityListActivity.java
@@ -1,13 +1,44 @@
 package com.example.jeppe.weatherapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CityListActivity extends AppCompatActivity {
 
+    Button btnRefresh;
+    Button btnAdd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_city_list);
+
+        btnRefresh = findViewById(R.id.btnRefresh);
+        btnRefresh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                refresh();
+            }
+        });
+
+        btnAdd = findViewById(R.id.btnAdd);
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addCityToList();
+            }
+        });
+    }
+
+    private void refresh() {
+        //Test code for UI
+        Intent intent = new Intent(this, CityDetailsActivity.class);
+        startActivity(intent);
+    }
+
+    private void addCityToList() {
+
     }
 }
